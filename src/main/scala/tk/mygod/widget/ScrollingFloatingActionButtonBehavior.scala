790eff2e0c6ed6fdb0bf71893cb1f6a2a5aa6de6
Mygod <mygodstudio@gmail.com>
2016-05-29 09:10:08 +0800

    Auto hide FloatingActionButton on scroll

---
+++ src/main/scala/tk/mygod/widget/ScrollingFloatingActionButtonBehavior.scala
@@ -0,0 +1,21 @@
+package tk.mygod.widget
+
+import android.content.Context
+import android.support.design.widget.{CoordinatorLayout, FloatingActionButton}
+import android.util.AttributeSet
+import android.view.View
+
+/**
+  * @author Mygod
+  */
+class ScrollingFloatingActionButtonBehavior(context: Context, attrs: AttributeSet)
+  extends FloatingActionButton.Behavior {
+  override def onStartNestedScroll(parent: CoordinatorLayout, child: FloatingActionButton, directTargetChild: View,
+                                   target: View, nestedScrollAxes: Int) = true
+  override def onNestedScroll(parent: CoordinatorLayout, child: FloatingActionButton, target: View, dxConsumed: Int,
+                              dyConsumed: Int, dxUnconsumed: Int, dyUnconsumed: Int) = {
+    super.onNestedScroll(parent, child, target, dxConsumed, dyConsumed, dxUnconsumed, dyUnconsumed)
+    val dy = dyConsumed + dyUnconsumed
+    if (dy < 0) child.show else if (dy > 0) child.hide
+  }
+}
